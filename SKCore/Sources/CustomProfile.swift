992879f54f15a0a3d62fa0c3080f320e1e92b9e0
Peter Zignego <pzignego@gmail.com>
2017-03-07 01:41:38 -0500

    Update copyright year

--- SKCore/Sources/CustomProfile.swift
+++ SKCore/Sources/CustomProfile.swift
@@ -1,7 +1,7 @@
 //
 // CustomProfile.swift
 //
-// Copyright © 2016 Peter Zignego. All rights reserved.
+// Copyright © 2017 Peter Zignego. All rights reserved.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
--- SKServer/Sources/Middleware/RedirectMiddleware.swift
+++ SKServer/Sources/Middleware/RedirectMiddleware.swift
@@ -1,7 +1,7 @@
 //
 // RedirectMiddleware.swift
 //
-// Copyright (c) 2016 Quark
+// Copyright (c) 2017 Quark
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
