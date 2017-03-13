ba6066875c246873cd66ab54144ce9d2761e4b95
Peter Zignego <pzignego@gmail.com>
2017-03-13 11:55:40 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@ let package = Package(
     name: "SlackKit",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/pvzig/swifter.git",
-                 majorVersion: 3, minor: 0),
+        .Package(url: "https://github.com/httpswift/swifter",
+                 majorVersion: 1, minor: 0),
         .Package(url: "https://github.com/daltoniam/Starscream", majorVersion: 2, minor: 0)
     ]
 )
